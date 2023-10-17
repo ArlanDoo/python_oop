caafb2dbd982d01ba12adeb905ef0cfb8e49bd8d
ButtHead1005 <kaliev1995ar@gmail.com>
2023-10-17 21:55:06 +0600

    Change claa and create object of class

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 class Pet:
     
     def __init__(self, pet_name, pet_type, pet_age, pet_gender):
-        self.name = pet_name
-        self.type = pet_type
-        self.age = pet_age
-        self.gender = pet_gender
+        self.__name = pet_name
+        self.__type = pet_type
+        self.__age = pet_age
+        self.__gender = pet_gender
     
     def get_info(self):
-            print(f"Name: {self.name}")
-            print(f"Type: {self.type}")
-            print(f"Gender: {self.gender}")
-            print(f"Age: {self.age}")
+            print(f"Name: {self.__name}")
+            print(f"Type: {self.__type}")
+            print(f"Gender: {self.__gender}")
+            print(f"Age: {self.__age}")
             
 class Dog(Pet):
     
-    def __init__(self, dog_name, dog_type, dog_age, dog_gender, dog_breed):
-        super().__init__(dog_name, dog_type, dog_age, dog_gender)
-        self._breed = dog_breed
+    breed = "Sheepdog"
     
-    def barking():
+    def __init__(self, dog_name, dog_type, dog_age, dog_gender, dog_breed=breed):
+        super().__init__(dog_name, dog_type, dog_age, dog_gender)
+        self.__breed = dog_breed
+    
+    def barking(self):
         print("Gaw-gaw-gaw!!!")
 
 
 class Cat(Pet):
     
-    def __init__(self, cat_name, cat_type, cat_age, cat_gender, cat_coat):
-        super().__init__(cat_name, cat_type, cat_age, cat_gender)
-        self._coat = cat_coat
+    coat = "Black"
     
-    def meow():
+    def __init__(self, cat_name, cat_type, cat_age, cat_gender, cat_coat=coat):
+        super().__init__(cat_name, cat_type, cat_age, cat_gender)
+        self.__coat = cat_coat
+    
+    def meow(self):
         print("Meow-meow-meow!")
+        
+dog = Dog("Buddy", "Male", 2, "Golden Retriever")
+cat = Cat("Fluffy", "Female", 3, "Calico", "Range")
+
+dog.barking()
